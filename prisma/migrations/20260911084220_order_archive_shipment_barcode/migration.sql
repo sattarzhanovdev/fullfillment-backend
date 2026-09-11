795a595345de9fb23f7a76be3317f9a8b4be9405
sattarzhanovdev <sattarzhanovdev@gmail.com>
2026-09-11 17:23:37 +0600

    Сделать миграцию barcode безопасной для существующих отгрузок (бэкафилл вместо NOT NULL сразу)

--- prisma/migrations/20260911084220_order_archive_shipment_barcode/migration.sql
+++ prisma/migrations/20260911084220_order_archive_shipment_barcode/migration.sql
@@ -2,7 +2,12 @@
 ALTER TABLE "MarketplaceOrder" ADD COLUMN     "archivedAt" TIMESTAMP(3);
 
 -- AlterTable
-ALTER TABLE "Shipment" ADD COLUMN     "barcode" TEXT NOT NULL;
+ALTER TABLE "Shipment" ADD COLUMN     "barcode" TEXT;
+
+-- Бэкафилл для уже существующих отгрузок (случайный 12-значный штрихкод, как генерирует приложение)
+UPDATE "Shipment" SET "barcode" = lpad(floor(random() * 1000000000000)::text, 12, '0') WHERE "barcode" IS NULL;
+
+ALTER TABLE "Shipment" ALTER COLUMN "barcode" SET NOT NULL;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Shipment_barcode_key" ON "Shipment"("barcode");
